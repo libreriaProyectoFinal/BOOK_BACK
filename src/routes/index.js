80f620af1ad5bd5d9ea299f03c38fdea3cec0463
theinventor1 <37818640+theinventor1@users.noreply.github.com>
2023-07-26 22:39:06 -0400

    qq

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,13 +6,10 @@ const { obtenerLibroPorId } = require('../controllers/libro/obtenerLibroPorId.js
 const { obtenerLibrosPorTitulo } = require('../controllers/libro/obtenerLibrosPorTitulo.js');
 const { obtenerLibrosPorGenero } = require('../controllers/libro/obtenerLibrosPorGenero.js');
 // ------------- matheus -----------------
-// -usuarios
+
 // const { handlerTodosUsuarios, handlerUsuarioPorId, handleCrearUsuario } = require("../controllers/usuario/handler/handlerUsuario.js");
-// -authLocal
-
-//const { autenticacionLocalUsuario } = require('../controllers/autenticacionLocal/autenticacionLocalUsuario.js')
-
-//const { autenticacionLocalUsuario } = require('../controllers/autenticacionLocal/autenticacionLocalUsuario.js')
+// const { autenticacionLocalUsuario } = require('../controllers/autenticacionLocal/autenticacionLocalUsuario.js')
+// const { autenticacionLocalUsuario } = require('../controllers/autenticacionLocal/autenticacionLocalUsuario.js')
 
 // ---------------------------------------
 
@@ -20,18 +17,12 @@ const { obtenerLibrosPorGenero } = require('../controllers/libro/obtenerLibrosPo
 // -libros
 const { actualizarLibro } = require('../controllers/libro/actualizarLibro.js');
 const { borradoLibro } = require('../controllers/libro/borradoLibro.js');
-// -------------------------------------------------
 
-// ------------------------- felipe ----------------
-// -libros
-
-// -autores
 const { obtenerAutores } = require('../controllers/autores/obtenerAutores.js');
 const { obtenerAutorPorNombre } = require('../controllers/autores/obtenerAutorPorNombre.js');
 const { obtenerAutorPorId } = require('../controllers/autores/obtenerAutorPorId.js');
-// -generos
+
 const { obtenerGeneros } = require('../controllers/generos/obtenerGeneros.js');
-// -----------------------------------------
 
 const router = Router();
 
@@ -39,20 +30,20 @@ router.post('/agregaLibro',agregaLibro );
 router.get('/obtenerLibros', obtenerLibros);
 router.get("/obtenerLibroId/:idl", obtenerLibroPorId); 
 // ---------------- matheus ------------------
-<<<<<<< HEAD
-router.get("/usuarios", handlerTodosUsuarios);
-router.get("/:idusuario", handlerUsuarioPorId);
-router.post("/crearUsuario", handleCrearUsuario);
-router.post("/login", autenticacionLocalUsuario);
-=======
+
+// router.get("/usuarios", handlerTodosUsuarios);
+// router.get("/:idusuario", handlerUsuarioPorId);
+// router.post("/crearUsuario", handleCrearUsuario);
+// router.post("/login", autenticacionLocalUsuario);
+
 //router.get("/usuarios", handlerTodosUsuarios);
 //router.get("/:idusuario", handlerUsuarioPorId);
 //router.post("/crearUsuario", handleCrearUsuario);
 //router.post("/login", autenticacionLocalUsuario);
->>>>>>> d46b5ab (qq)
+
 // -------------------------------------------
 
-// ---------------- waldir -------------------
+// ---------------- waldir ------------------- 
 router.delete("/borradoLibro/:idlibro", borradoLibro);
 router.put("/actualizarLibro/:idlibro", actualizarLibro);
 // -------------------------------------------
