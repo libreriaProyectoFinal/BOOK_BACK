9c652bbf229502c12481626b8ad62a97e2c91f02
Waldir merjildo <muwaldir@gmail.com>
2023-07-27 13:01:33 -0500

    actualizacion de controller borradolibro

--- src/controllers/libro/borradoLibro.js
+++ src/controllers/libro/borradoLibro.js
@@ -3,6 +3,33 @@ const { Libro } = require('../../db.js');
 const borradoLibro = async (req, res) => {
   const { idlibro } = req.params;
  try {
+
+   // Buscar el libro por su id
+   const libro = await Libro.findOne({
+    where: { idlibro },
+  });
+
+  
+
+  if (!libro) {
+    return res.status(404).json({ error: 'Libro no encontrado' });
+  }
+
+  // Verificar si el libro pertenece a un género
+  if (libro.nombregenero) {
+    // Obtener la cantidad de libros del mismo género que no han sido borrados
+    const cantidadLibrosGenero = await Libro.count({
+      where: { nombregenero: libro.nombregenero, esborrado: 0 },
+    });
+    
+    // Si solo hay un libro del mismo género y no ha sido borrado, no se actualiza el atributo esborrado
+    if (cantidadLibrosGenero === 1 && libro.esborrado === 0) {
+   
+      return res.status(400).json({ error: 'No se puede borrar el único libro de este género' });
+    }
+  }
+
+  // Actualizar el atributo esborrado del libro
   const [numRowsUpdated, [updatedLibro]] = await Libro.update(
     { esborrado: 1 }, // Datos que se actualizarán
     {
