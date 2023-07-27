0f0a85c18e6c8264c6781321c0c79a6419e1a72e
theinventor1 <37818640+theinventor1@users.noreply.github.com>
2023-07-27 14:07:19 -0400

    2

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,24 +2,19 @@ const { Router } = require('express');
 
 const { agregaLibro } = require('../controllers/libro/agregaLibro.js');
 const { obtenerLibros } = require('../controllers/libro/obtenerLibros.js');
-
-// ------------- matheus -----------------
-// -usuarios
- const { handlerTodosUsuarios, handlerUsuarioPorId, handleCrearUsuario } = require("../controllers/usuario/handler/handlerUsuario.js");
-// // -authLocal
- const { autenticacionLocalUsuario } = require('../controllers/autenticacionLocal/autenticacionLocalUsuario.js')
-// ---------------------------------------
-// ----------------------- waldir ------------------
-// -libros
-const { actualizarLibro } = require('../controllers/libro/actualizarLibro.js');
-const { borradoLibro } = require('../controllers/libro/borradoLibro.js');
-// -------------------------------------------------
-
-// ------------------------- felipe ----------------
-// -libros
 const { obtenerLibroPorId } = require('../controllers/libro/obtenerLibroPorId.js');
 const { obtenerLibrosPorTitulo } = require('../controllers/libro/obtenerLibrosPorTitulo.js');
 const { obtenerLibrosPorGenero } = require('../controllers/libro/obtenerLibrosPorGenero.js');
+// ------------- matheus -----------------
+// -usuarios
+ //const { handlerTodosUsuarios, handlerUsuarioPorId, handleCrearUsuario } = require("../controllers/usuario/handler/handlerUsuario.js");
+// // -authLocal
+ //const { autenticacionLocalUsuario } = require('../controllers/autenticacionLocal/autenticacionLocalUsuario.js')
+// ---------------------------------------
+// -libros
+const { actualizarLibro } = require('../controllers/libro/actualizarLibro.js');
+const { borradoLibro } = require('../controllers/libro/borradoLibro.js');
+
 // -autores
 const { obtenerAutores } = require('../controllers/autores/obtenerAutores.js');
 const { obtenerAutorPorNombre } = require('../controllers/autores/obtenerAutorPorNombre.js');
@@ -36,6 +31,8 @@ const  { handlePaymentNotification, receiveWebhook } = require("../controllers/s
 const router = Router();
 
 router.get("/obtenerGeneros", obtenerGeneros);
+router.get("/obtenerLibrosPorTitulo", obtenerLibrosPorTitulo);
+router.get("/obtenerLibrosPorGenero", obtenerLibrosPorGenero);
 
 //---------------sistema de pago
 router.post('/generar-orden', creaOCyDetalle );   /**aqui oc y detalle OK  */
@@ -46,10 +43,10 @@ router.get('/obtenerLibros', obtenerLibros);
 router.get("/obtenerLibroId/:idl", obtenerLibroPorId); 
 
 // ---------------- matheus ------------------
-  router.get("/usuarios", handlerTodosUsuarios);
-  router.get("/:idusuario", handlerUsuarioPorId);
-  router.post("/crearUsuario", handleCrearUsuario);
-  router.post("/login", autenticacionLocalUsuario);
+  // router.get("/usuarios", handlerTodosUsuarios);
+  // router.get("/:idusuario", handlerUsuarioPorId);
+  // router.post("/crearUsuario", handleCrearUsuario);
+  // router.post("/login", autenticacionLocalUsuario);
 // -------------------------------------------
 // ---------------- waldir -------------------
 router.delete("/borradoLibro/:idlibro", borradoLibro);
@@ -57,8 +54,6 @@ router.put("/actualizarLibro/:idlibro", actualizarLibro);
 // -------------------------------------------
 // ---------------- felipe -------------------
 
-router.get("/obtenerLibrosPorTitulo", obtenerLibrosPorTitulo);
-router.get("/obtenerLibrosPorGenero", obtenerLibrosPorGenero);
 router.get("/obtenerAutores", obtenerAutores);
 router.get("/obtenerAutorNombre/:nombre", obtenerAutorPorNombre);
 router.get("/obtenerAutorId/:ida", obtenerAutorPorId); 
